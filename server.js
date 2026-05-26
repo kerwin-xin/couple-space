0d83770d82790d6a1087d79d76412cc6a30ac639
kerwin-xin <kerwin@users.noreply.github.com>
2026-05-26 20:25:40 +0800

    Add Render deploy config, use PORT env var

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3456;
+const PORT = process.env.PORT || 3456;
 const DATA_FILE = path.join(__dirname, 'data.json');
 const MIME = {
   '.html': 'text/html; charset=utf-8',
